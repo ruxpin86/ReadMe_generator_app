c805aa7049135cedb91392d92636071681beb26f
Ted Glynn <will.glynn@gmail.com>
2022-05-26 18:04:04 -0600

    initial commit for readme generator

---
+++ index.js
@@ -0,0 +1,25 @@
+// TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
+
+// TODO: Create an array of questions for user input
+const questions = [
+  {
+    type: "input",
+    name: "title",
+    message: "What is your projects title?",
+  },
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {
+  inquirer.prompt(questions).then((response) => {
+    console.log(response);
+  });
+}
+// Function call to initialize app
+init();
